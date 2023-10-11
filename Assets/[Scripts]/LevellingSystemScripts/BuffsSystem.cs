958146c6a81c0b6d6d6ec33810bceb0ec2cdd19b
Ikamjot Hundal <ihundal@my.centennialcollege.ca>
2023-10-11 11:27:30 -0400

    [ADD] Added a folder dedicated to Levelling & added Buff System
    Buff Systems is only commented as steps for now.

---
+++ Assets/[Scripts]/LevellingSystemScripts/BuffsSystem.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuffsSystem : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Call the Level-Up script
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    // Call the Stats 
+
+    // Check if the levelledUp is true from levelling System. 
+    // Two approaches for now.
+    // Create a method for each type of type.
+    // or just increase numbers for each types of stats in one method.
+
+    // I believes that it will be triggered via the levelled-up menu 
+    // So click the button for buff increase--  which then will trigger the method.  
+
+}
