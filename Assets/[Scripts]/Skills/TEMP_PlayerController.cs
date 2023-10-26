ce4267d5bfef136c3b6a4516714d86f0883ef836
Marcus Ngooi <mngooi@gmail.com>
2023-10-26 15:02:35 -0400

    CT-17 Set up initial managers and window presenter
    Managers:
    -PlayerController
    -SkillManager
    -ExperienceManager
    Base Classes:
    -Weapon
    -Buff
    Presenter:
    -LevelUpWindowPresenter

---
+++ Assets/[Scripts]/Skills/TEMP_PlayerController.cs
@@ -0,0 +1,34 @@
+/*  Author's Name:          Marcus Ngooi
+ *  Last Modified By:       Marcus Ngooi
+ *  Date Last Modified:     October 26, 2023
+ *  Program Description:    Controls the player.
+ *  Revision History:       October 26, 2023: Initial PlayerController script.
+ */
+
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TEMP_PlayerController : MonoBehaviour
+{
+    public List<TEMP_Weapon> weapons = new();
+    public List<TEMP_Buff> buffs = new();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void AddWeapon(TEMP_Weapon weapon)
+    {
+        weapons.Add(weapon);
+    }
+    public void AddBuff(TEMP_Buff buff)
+    {
+        buffs.Add(buff);
+    }
+}
