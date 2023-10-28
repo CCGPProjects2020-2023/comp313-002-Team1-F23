09a040a2ce2b82f249570fe066a25af6860c8252
Ikamjot Hundal <ihundal@my.centennialcollege.ca>
2023-10-28 14:54:52 -0400

    Readded the Scripts for Buffs

--- Assets/[Scripts]/Skills/TEMP_Buff.cs
+++ Assets/[Scripts]/Skills/TEMP_Buff.cs
@@ -1,16 +1,28 @@
 /*  Author's Name:          Marcus Ngooi
- *  Last Modified By:       Marcus Ngooi
+ *  Last Modified By:       Ikamjot Hundal
  *  Date Last Modified:     October 26, 2023
  *  Program Description:    Base class for a Buff.
  *  Revision History:       October 26, 2023: Initial Buff script.
+ *                          October 26, 2023: Added features for dealing with child classes
  */
 
 
 public class TEMP_Buff : Skill
 {
     public BuffType Type { get; private set; }
+
+    //public 
+
     public TEMP_Buff(BuffType type, int maxLevel) : base(type.ToString(), maxLevel)
     {
         this.Type = type;
+    }
+
+    public void ApplyBuff()
+    {
+        if (Type == BuffType.Heart)
+        {
+            //print()
+        }
     }
 }
