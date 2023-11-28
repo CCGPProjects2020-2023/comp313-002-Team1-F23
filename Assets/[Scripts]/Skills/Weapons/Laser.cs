7218959b6537f34809bb067a3d896a5a687ab8b6
Mithul <77376534+MithulPy@users.noreply.github.com>
2023-11-28 01:59:24 -0500

    [Updated Weapon Evolutions+ Evolved Laser + MissileLauncher]

--- Assets/[Scripts]/Skills/Weapons/Laser.cs
+++ Assets/[Scripts]/Skills/Weapons/Laser.cs
@@ -11,13 +11,22 @@ public class Laser : MonoBehaviour
 {
     [SerializeField] private LaserGun laserGun;
     [SerializeField] private float lifespan = 10f;
+
     private void Awake()
     {
         laserGun = GameObject.FindWithTag("LaserGun").GetComponent<LaserGun>();
         Rigidbody2D rBody = GetComponent<Rigidbody2D>();
-
-        // baseProjectileSpeed is temp. Must account for base + upgrade
         rBody.velocity = laserGun.WeaponSO.baseProjectileSpeed * transform.up;
         Destroy(gameObject, lifespan);
+    }
+
+    public void SetEvolvedProperties()
+    {
+
+        SpriteRenderer laserSpriteRenderer = GetComponent<SpriteRenderer>();
+        if (laserSpriteRenderer != null)
+        {
+            laserSpriteRenderer.color = Color.red; // Change to red for evolved state
+        }
     }
 }
---
+++ Assets/[Scripts]/Skills/Weapons/Missile.cs
@@ -0,0 +1,32 @@
+/** Author's Name:          Mithul Koshy
+ *  Last Modified By:       Mithul Koshy
+ *  Date Last Modified:     November 28, 2023
+ *  Program Description:    A script to handle the Missile.
+ *  Revision History:       November 28, 2023 (Mithul Koshy): 
+ */
+
+using UnityEngine;
+
+public class Missile : MonoBehaviour
+{
+    [SerializeField] private MissileLauncher missileLauncher;
+    [SerializeField] private float lifespan = 15f;
+
+    private void Awake()
+    {
+        missileLauncher = GameObject.FindWithTag("MissileLauncher").GetComponent<MissileLauncher>();
+        Rigidbody2D rBody = GetComponent<Rigidbody2D>();
+        rBody.velocity = missileLauncher.WeaponSO.baseProjectileSpeed * transform.up;
+        Destroy(gameObject, lifespan);
+    }
+
+    public void SetEvolvedProperties()
+    {
+        SpriteRenderer missileSpriteRenderer = GetComponent<SpriteRenderer>();
+        if (missileSpriteRenderer != null)
+        {
+            missileSpriteRenderer.color = Color.blue; // Change to blue for evolved state
+        }
+
+    }
+}
