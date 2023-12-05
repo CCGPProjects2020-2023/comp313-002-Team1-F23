c79e42548f10036f9fcab0917a902dd20907dd02
Ikamjot Hundal <ihundal@my.centennialcollege.ca>
2023-12-05 15:51:24 -0500

    [MOD] readjusted the player's hud handler script

--- Assets/[Scripts]/UIHandlers/PlayerHUDHandler.cs
+++ Assets/[Scripts]/UIHandlers/PlayerHUDHandler.cs
@@ -18,37 +18,71 @@ public class PlayerHUDHandler : MonoBehaviour
     [SerializeField] GameObject timerTextGO;
     TextMeshProUGUI timerText;
 
+    [SerializeField] GameObject enemiesTextGO;
+    TextMeshProUGUI enemiesText;
+
     [Header("Altering the Time ")]
     [SerializeField] private float timeElapsed = 0;
     [SerializeField] private bool timeRunning = false;
     [Header("Multiply by 100")]
     [SerializeField] private float maxTime = 600f;
 
+    private int enemiesKilled;
+
+    private float timeResult; 
+
+
+
+    // Temporary Measure
+    private void Awake()
+    {
+        if (PlayerPrefs.HasKey("EnemiesKilled") && PlayerPrefs.HasKey("MaxTime"))
+        {
+            enemiesKilled = PlayerPrefs.GetInt("EnemiesKilled");
+            timeResult = PlayerPrefs.GetFloat("MaxTime");
+        }
+        else
+        {
+            PlayerPrefs.SetInt("EnemiesKilled", enemiesKilled);
+            PlayerPrefs.SetFloat("MaxTime", timeResult);
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        timerText = timerTextGO.GetComponent<TextMeshProUGUI>();        
-        timeRunning = true;
+        timerText = timerTextGO.GetComponent<TextMeshProUGUI>();
+        enemiesText = enemiesTextGO.GetComponent<TextMeshProUGUI> ();
+
+        enemiesText.text = "Enemies Killed: " + enemiesKilled.ToString();
+
+        DisplayTime(timeResult, timerText);
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (timeRunning)
+        if (timeRunning == true)
         {
             if (timeElapsed <= maxTime)
             {
                 timeElapsed += Time.deltaTime;
                 PlayerPrefs.SetFloat("MaxTime", timeElapsed);
                 DisplayTime(timeElapsed, timerText);
+
+                enemiesText.text = $"Enemies Killed: {GameController.Instance.enemiesKilledCounter}";
+
             }
             else
             {
                 timeElapsed = maxTime;
+                enemiesText.text = $"Enemies Killed: {GameController.Instance.enemiesKilledCounter}";
                 PlayerPrefs.SetFloat("MaxTime", timeElapsed);
                 SceneManager.LoadScene("GameOver");
             }
-        }        
+        }
     }
 
     public void DisplayTime(float timerDisplay, TextMeshProUGUI timerText)
