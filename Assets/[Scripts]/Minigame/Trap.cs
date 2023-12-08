ab8ef0f5053c4174003252cb97a6f83c9a41d868
Yusuke <ykuroki@my.centennialcollege.ca>
2023-12-08 16:17:52 -0500

    changed Trap mechanics

--- Assets/[Scripts]/Minigame/Trap.cs
+++ Assets/[Scripts]/Minigame/Trap.cs
@@ -1,4 +1,3 @@
-using System.Diagnostics;
 /** Author's Name:          Han Bi
  *  Last Modified By:       Han Bi
  *  Date Last Modified:     November 17, 2023
@@ -15,35 +14,45 @@ using UnityEngine;
 public class Trap : Minigame
 {
 	public string playerTag = "Player";
-	public EnemyFactory enemyFactory;
-	[Range(1, 100)]
+	[Range(0, 100)]
 	public int maxEmemySpawn = 5;
 	[Range(0.0f, 100.0f)]
 	public float spawnDistance = 5.0f;
+	[Range(0, 100)]
+	public int extendCollider = 6;
+	public BoxCollider2D trapArea;
+	private GameObject trapSprite;
+	private EnemyFactory enemyFactory;
 	private int countEnemy;
 	private bool isTrapOn = false;
+    GameObject player;
+
+    public void Awake()
+    {
+        if (player == null)
+        {
+            player = GameObject.FindGameObjectWithTag(playerTag);
+        }
+    }
+
 
 	void Start()
 	{
 		enemyFactory = GameObject.Find("EnemyFactory").GetComponent<EnemyFactory>();
-	}
-
-	void Update()
-	{
-		if (isTrapOn)
-		{
-			finishTrap();
-		}
+		trapArea = GetComponent<BoxCollider2D>();
+		trapSprite = this.gameObject;
 	}
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (!isTrapOn && other.CompareTag(playerTag))
+        if (!this.isTrapOn && other.CompareTag(playerTag))
         {
-			// Spawn enemies randomly
-			isTrapOn = true;
+			trapArea.size = new Vector2(extendCollider, extendCollider);
+			trapSprite.GetComponent<SpriteRenderer>().enabled = false;
+
+			this.isTrapOn = true;
 			Enemy.EnemyType enemy;
-			countEnemy = UnityEngine.Random.Range(1, maxEmemySpawn);
+			countEnemy = UnityEngine.Random.Range(0, maxEmemySpawn);
 			Vector2 position = new Vector2();
 
 			for (int i = 0; i <= countEnemy; i++)
@@ -52,15 +61,27 @@ public class Trap : Minigame
 				position.x = UnityEngine.Random.Range(this.transform.position.x - spawnDistance, this.transform.position.x + spawnDistance);
 				position.y = UnityEngine.Random.Range(this.transform.position.y - spawnDistance, this.transform.position.y + spawnDistance);
 
-				enemyFactory.CreateEnemy(enemy, position);
-			}		
+				var obj = enemyFactory.CreateEnemy(enemy, position);
+				obj.GetComponent<Enemy>().SetTarget(player);
+			}
         }
     }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+		if (other.CompareTag(playerTag))
+		{
+			if (this.isTrapOn)
+			{
+				finishTrap();
+			}
+		}
+	}
 
 	private void finishTrap()
 	{
 		// Finish minigame
-		isTrapOn = false;
+		this.isTrapOn = false;
 		countEnemy = 0;
 		CompleteMinigame();
 		Destroy(gameObject, 1);
